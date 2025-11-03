5a4dacee652cdd3f7d381e2ba5406f573cd18f3c
Vladyslav <154080171+metimol@users.noreply.github.com>
2025-11-03 20:18:43 +0100

    Add new categories icons

--- app/src/main/java/com/metimol/easybook/MainViewModel.java
+++ app/src/main/java/com/metimol/easybook/MainViewModel.java
@@ -1,9 +1,10 @@
 package com.metimol.easybook;
 
+import android.app.Application;
 import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
-import androidx.lifecycle.ViewModel;
 
 import com.metimol.easybook.api.ApiClient;
 import com.metimol.easybook.api.ApiService;
@@ -19,13 +20,17 @@ import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
-public class MainViewModel extends ViewModel {
+public class MainViewModel extends AndroidViewModel {
     private final MutableLiveData<Integer> statusBarHeight = new MutableLiveData<>();
     private final MutableLiveData<List<Category>> categories = new MutableLiveData<>();
     private final MutableLiveData<List<Book>> books = new MutableLiveData<>();
     private int currentPage = 0;
     private boolean isLoading = false;
     private boolean isLastPage = false;
+
+    public MainViewModel(@NonNull Application application) {
+        super(application);
+    }
 
     public void setStatusBarHeight(int height) {
         statusBarHeight.setValue(height);
@@ -45,30 +50,36 @@ public class MainViewModel extends ViewModel {
 
     public void fetchCategories() {
         List<Category> categoryList = new ArrayList<>();
+        String[] categoryNames = getApplication().getResources().getStringArray(R.array.category_names);
+        int[] categoryIcons = {
+                R.drawable.ic_category_fantasy,
+                R.drawable.ic_category_thriller,
+                R.drawable.ic_category_drama,
+                R.drawable.ic_category_business,
+                R.drawable.ic_category_biography,
+                R.drawable.ic_category_children,
+                R.drawable.ic_category_history,
+                R.drawable.ic_category_classic,
+                R.drawable.ic_category_health,
+                R.drawable.ic_globe,
+                R.drawable.ic_category_nonfiction,
+                R.drawable.ic_category_education,
+                R.drawable.ic_category_poetry,
+                R.drawable.ic_category_adventure,
+                R.drawable.ic_category_psychology,
+                R.drawable.ic_category_miscellaneous,
+                R.drawable.ic_category_ranobe,
+                R.drawable.ic_category_religion,
+                R.drawable.ic_category_novel,
+                R.drawable.ic_category_crime,
+                R.drawable.ic_category_horror,
+                R.drawable.ic_category_esoteric,
+                R.drawable.ic_category_humor
+        };
 
-        categoryList.add(new Category("1", "Fantasy", R.drawable.ic_category_fantasy));
-        categoryList.add(new Category("2", "Thriller", R.drawable.ic_category_fantasy));
-        categoryList.add(new Category("3", "Drama", R.drawable.ic_category_fantasy));
-        categoryList.add(new Category("4", "Business Growth", R.drawable.ic_category_fantasy));
-        categoryList.add(new Category("5", "Biography", R.drawable.ic_category_fantasy));
-        categoryList.add(new Category("6", "Children", R.drawable.ic_category_fantasy));
-        categoryList.add(new Category("7", "History Culture", R.drawable.ic_category_fantasy));
-        categoryList.add(new Category("8", "Classic", R.drawable.ic_category_fantasy));
-        categoryList.add(new Category("9", "Health", R.drawable.ic_category_fantasy));
-        categoryList.add(new Category("10", "Foreign Language", R.drawable.ic_category_fantasy));
-        categoryList.add(new Category("11", "Non-Fiction", R.drawable.ic_category_fantasy));
-        categoryList.add(new Category("12", "Education", R.drawable.ic_category_fantasy));
-        categoryList.add(new Category("13", "Poetry", R.drawable.ic_category_fantasy));
-        categoryList.add(new Category("14", "Adventure", R.drawable.ic_category_fantasy));
-        categoryList.add(new Category("15", "Psychology", R.drawable.ic_category_fantasy));
-        categoryList.add(new Category("16", "Miscellaneous", R.drawable.ic_category_fantasy));
-        categoryList.add(new Category("17", "Ranobe", R.drawable.ic_category_fantasy));
-        categoryList.add(new Category("18", "Religion", R.drawable.ic_category_fantasy));
-        categoryList.add(new Category("19", "Novel", R.drawable.ic_category_fantasy));
-        categoryList.add(new Category("20", "True Crime", R.drawable.ic_category_fantasy));
-        categoryList.add(new Category("21", "Horror", R.drawable.ic_category_fantasy));
-        categoryList.add(new Category("22", "Esoteric", R.drawable.ic_category_fantasy));
-        categoryList.add(new Category("23", "Humor", R.drawable.ic_category_fantasy));
+        for (int i = 0; i < categoryNames.length; i++) {
+            categoryList.add(new Category(String.valueOf(i + 1), categoryNames[i], categoryIcons[i]));
+        }
 
         categories.setValue(categoryList);
     }
