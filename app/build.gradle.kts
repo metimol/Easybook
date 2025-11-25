49c2fc7dc0d7a6889a1f76404572bd8bfe1f8385
Vladyslav <154080171+metimol@users.noreply.github.com>
2025-11-25 15:46:29 +0100

    Bump version to 1.0.6

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "com.metimol.easybook"
         minSdk = 29
         targetSdk = 36
-        versionCode = 6
-        versionName = "1.0.5"
+        versionCode = 7
+        versionName = "1.0.6"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
