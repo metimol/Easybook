d25afc917a03fde2aad98fcba3f183e355ee276f
Vladyslav <154080171+metimol@users.noreply.github.com>
2025-12-20 13:52:53 +0100

    Refactor ABI split configuration

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,7 +50,8 @@ android {
         abi {
             isEnable = true
             reset()
-            include("armeabi-v7a", "arm64-v8a", "x86_64", "x86")
+            include("armeabi-v7a", "arm64-v8a")
+            isUniversalApk = true
         }
     }
 
